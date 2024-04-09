1b00687966441833a3d9509de0c58cbeed166e61
Cozel <Cozel@DESKTOP-ETQ7VGK>
2024-04-09 20:49:48 +0300

    Tema Joi  04.04.2024

---
+++ Methods (Exercitii)/Exercitii3/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+#region Exercitii 2
+public class Program
+{
+    
+    public static int GetMin(int a, int b)
+    {
+        return Math.Min(a, b);
+    }
+
+    public static float GetMin(float a, float b)
+    {
+        return Math.Min(a, b);
+    }
+
+    public static decimal GetMin(decimal a, decimal b)
+    {
+        return Math.Min(a, b);
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number:");
+        decimal num1 = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        decimal num2 = decimal.Parse(Console.ReadLine());
+
+        decimal min = GetMin(num1, num2);
+        Console.WriteLine($"The smallest of the two numbers is: {min}");
+    }
+}
+#endregion
