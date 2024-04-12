f084893576496948aa715572e29d8cdd46a251b8
Cozel <Cozel@DESKTOP-ETQ7VGK>
2024-04-12 20:37:13 +0300

    obfuscated mail

---
+++ ObfuscateEmail/ObfuscateEmail/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string email = "awesome@dotnet.com";
+        string obfuscatedEmail = ObfuscateEmail(email);
+        Console.WriteLine(obfuscatedEmail);
+    }
+    static string ObfuscateEmail(string email)
+    {
+        int  indexOfSign= email.IndexOf("@");
+        string username = email.Substring(0, indexOfSign);
+        string domain = email.Substring(indexOfSign + 1);
+
+        string obfuscatedUsername = new string('*', username.Length);
+        return obfuscatedUsername + "@" + domain;
+    }
+}
