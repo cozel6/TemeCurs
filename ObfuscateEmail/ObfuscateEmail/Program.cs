c896a18c1129d073da6ce375cac1e0e36f412cb5
Cozel <Cozel@DESKTOP-ETQ7VGK>
2024-04-15 21:24:58 +0300

    Varianta2 Regex.Replace

--- ObfuscateEmail/ObfuscateEmail/Program.cs
+++ ObfuscateEmail/ObfuscateEmail/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 class Program
 {
     static void Main(string[] args)
     {
         string email = "awesome@dotnet.com";
-        string obfuscatedEmail = ObfuscateEmail(email);
+        string obfuscatedEmail = ObfuscateEmail2(email);
         Console.WriteLine(obfuscatedEmail);
     }
     static string ObfuscateEmail(string email)
@@ -15,6 +16,13 @@ class Program
         string domain = email.Substring(indexOfSign + 1);
 
         string obfuscatedUsername = new string('*', username.Length);
+
         return obfuscatedUsername + "@" + domain;
+    }
+    static string ObfuscateEmail2(string email)
+    {
+        string pattern = @"^(.*?)(?=@)";
+        string obfuscatedEmail = Regex.Replace(email, pattern, m => new string('*', m.Length));
+        return obfuscatedEmail;
     }
 }
