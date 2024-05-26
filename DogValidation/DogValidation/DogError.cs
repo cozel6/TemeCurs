11d12761b880dd4dee1a6bbcc8aecd52aceec315
Cozel <Cozel@DESKTOP-ETQ7VGK>
2024-05-26 20:50:06 +0300

    👆 Dog With Exceptions

---
+++ DogValidation/DogValidation/DogError.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogValidation
+{
+    public class DogError
+    {
+
+        private int age;
+        private string name;
+
+
+        public int Age
+        {
+            get 
+            {
+                return age; 
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Varsta nu poate avea valoare negaiva");
+                }
+                if (value > 100)
+                {
+                    throw new ArgumentException("Varsta nu poate avea valoarea peste 100");
+                }
+                age = value;
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException("Numele nu poate contine mai putin de 2 caractere");
+                }
+                name = value;
+            }
+        }
+        
+        public DogError() {}
+
+        public DogError(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+    }
+}
---
+++ DogValidation/DogValidation/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Cache;
+
+namespace DogValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //negative case 1 : age < 0
+            try
+            {
+                DogError dogError = new DogError
+                {
+                    Age = -5,  
+                    Name = "Rex"
+                };
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            // negative case 2 : age > 100
+            try
+            {
+                DogError dogError2 = new DogError
+                {
+                    Age = 101,  
+                    Name = "Rex"
+                };
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            //negative case 3 : name < 2 ch
+            try
+            {
+                DogError dogError = new DogError
+                {
+                    Age = 5,  
+                    Name = "R" // trigger 
+                };
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            // positive obj
+            try
+            {
+                DogError dogError = new DogError
+                {
+                    Age = 15,
+                    Name = "Rex"  
+                };
+                if( dogError.Age == 1) // in cazul in care cainele are varsta de 1 an :D
+                {
+                    Console.WriteLine($"Cainele are varsta de: {dogError.Age} an si numele de: {dogError.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"Cainele are varsta de: {dogError.Age} ani si numele de: {dogError.Name}");
+                }
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+    }
+}
